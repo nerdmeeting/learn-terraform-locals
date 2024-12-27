65dd8628764e6581f91d0825d4d622ad4e495250
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 17:09:14 -0700

    Display the tags.
    This output will display the tags used which are a combination of var.resource_tags and local.required_tags.

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "public_dns_name" {
   description = "Public DNS names of the load balancer for this project"
   value       = module.elb_http.elb_dns_name
 }
+
+output "tags" {
+  value = local.tags
+}
